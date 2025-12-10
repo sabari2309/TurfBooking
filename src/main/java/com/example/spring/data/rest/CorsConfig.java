5f34ae8a5d89b2083b0ea1077c833bbe9097d573
sabari2309 <sabareeswarangopal@gmail.com>
2025-12-10 20:12:22 +0530

    config

--- src/main/java/com/example/spring/data/rest/CorsConfig.java
+++ src/main/java/com/example/spring/data/rest/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.example.spring.data.rest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
