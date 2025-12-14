7316161ab37377eedad1192310924bfcd2ea43b1
sabari2309 <sabareeswarangopal@gmail.com>
2025-12-14 17:10:55 +0530

    mail change

--- src/main/java/com/example/spring/data/rest/repo/CustomerRepo.java
+++ src/main/java/com/example/spring/data/rest/repo/CustomerRepo.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface CustomerRepo extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String mail);
 }
